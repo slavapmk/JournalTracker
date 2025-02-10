f13e5fb3b1795e822f160c1d50d686ef222fef75
Slava Petrov <slavapmk@gmail.com>
2025-02-10 14:31:35 +0300

    New stubs

---
+++ app/src/main/java/ru/slavapmk/journaltracker/ui/StudentsEditActivity.kt
@@ -0,0 +1,31 @@
+package ru.slavapmk.journaltracker.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ru.slavapmk.journaltracker.R
+import ru.slavapmk.journaltracker.databinding.ActivityCampusEditBinding
+import ru.slavapmk.journaltracker.databinding.ActivityStudentsEditBinding
+import ru.slavapmk.journaltracker.ui.MainActivity.Companion.fmanager
+
+class StudentsEditActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityStudentsEditBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        fmanager = supportFragmentManager
+        binding = ActivityStudentsEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 5)
+            insets
+        }
+    }
+}
