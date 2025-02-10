214bdf22cc9ff8988932de5fdd556f98c6f5aecb
Slava Petrov <slavapmk@gmail.com>
2025-02-10 17:28:58 +0300

    Refactored layouts and added navigation
    - Changed the add button in `activity_campus_edit.xml` and `activity_time_edit.xml` to use `android:button="@null"` and manage inset values for a consistent look.
    - Modified `fragment_schedule.xml` to increase the size of the add lesson button.
    - Redesigned `activity_edit_lesson.xml` layout, adding a save button.
    - Added click listeners in `ScheduleFragment.kt` to navigate to `EditLessonActivity` when the add lesson button is clicked.
    - Added click listeners in `SettingsFragment.kt` for the lessons, campus, and students buttons, navigating to the respective activities.
    - Added the main id to `activity_campus_edit.xml` and `activity_edit_lesson.xml`
    - Removed unnecessary `item_student_edit` list item from `activity_students_edit.xml`.

--- app/src/main/java/ru/slavapmk/journaltracker/ui/ScheduleFragment.kt
+++ app/src/main/java/ru/slavapmk/journaltracker/ui/ScheduleFragment.kt
@@ -1,10 +1,12 @@
 package ru.slavapmk.journaltracker.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import ru.slavapmk.journaltracker.databinding.ActivityEditLessonBinding
 import ru.slavapmk.journaltracker.databinding.FragmentScheduleBinding
 
 class ScheduleFragment : Fragment() {
@@ -17,6 +19,12 @@ class ScheduleFragment : Fragment() {
     ): View {
         super.onCreate(savedInstanceState)
         binding = FragmentScheduleBinding.inflate(layoutInflater)
+
+        binding.addLessonButton.setOnClickListener {
+            val intent = Intent(activity, EditLessonActivity::class.java)
+            activity.startActivity(intent)
+        }
+
         return binding.root
     }
 }
