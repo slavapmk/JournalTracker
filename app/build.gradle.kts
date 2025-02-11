bffcebf576e7d5856bde0a0cc0a6beef9d7d86fd
Slava Petrov <slavapmk@gmail.com>
2025-02-11 14:30:59 +0300

    Refactor schedule week selection
    - Changed schedule_semester string to schedule_week in strings.xml and strings-ru.xml
    - Changed semester to week in fragment_schedule.xml
    - Changed binding.semester to binding.week in ScheduleFragment.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        versionCode = 3
-        versionName = "0.3"
+        versionCode = 4
+        versionName = "0.4"
         applicationId = "ru.slavapmk.journaltracker"
         minSdk = 29
         targetSdk = 34
