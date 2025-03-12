a584a5066dd588ed92766b93cb9727e7311ef1b1
Slava Petrov <slavapmk@gmail.com>
2025-03-12 17:31:19 +0300

    Refactor: Improve export week functionality
    - Added `StudentAttendance` data class to represent student attendance.
    - Modified `renderLessons` to calculate and render student attendance for each lesson.
    - Implemented logic to aggregate student attendance based on different attendance types.
    - Updated `renderLessons` to return a map of student attendance sums.
    - Added logic to display attendance status in `renderLessons`.
    - Improved logic to process and export student attendance data per lesson.
    - Updated `LessonAttendance` to `StudentAttendance` and adjusted its operations accordingly.
    - Implemented logic to load lessons and their attendance.
    - Added logic for the different types of skipping.
    - Updated `toEdit` extension function.
    - Updated `renderSummary` as `TODO` for now.

--- app/src/main/kotlin/ru/slavapmk/journalTracker/viewModels/ExportWeekViewModel.kt
+++ app/src/main/kotlin/ru/slavapmk/journalTracker/viewModels/ExportWeekViewModel.kt
@@ -6,18 +6,20 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.apache.poi.ss.usermodel.HorizontalAlignment
 import ru.slavapmk.journalTracker.R
+import ru.slavapmk.journalTracker.dataModels.StudentAttendanceLesson
+import ru.slavapmk.journalTracker.dataModels.toEdit
 import ru.slavapmk.journalTracker.excelExporter.BorderData
 import ru.slavapmk.journalTracker.excelExporter.CellData
 import ru.slavapmk.journalTracker.excelExporter.RenderData
 import ru.slavapmk.journalTracker.storageModels.StorageDependencies
 import ru.slavapmk.journalTracker.storageModels.entities.StudentEntity
 
-data class LessonAttendance(
+data class StudentAttendance(
     val respectful: Int = 0,
     val disrespectful: Int = 0
 ) {
-    operator fun plus(other: LessonAttendance): LessonAttendance {
-        return LessonAttendance(
+    operator fun plus(other: StudentAttendance): StudentAttendance {
+        return StudentAttendance(
             respectful + other.respectful,
             disrespectful + other.disrespectful
         )
@@ -35,9 +37,9 @@ class ExportWeekViewModel : ViewModel() {
         val (renderStudents, students) = renderStudentNames(context)
         result.add(renderStudents)
 
-        val attendances: MutableList<Map<Int, LessonAttendance>> = mutableListOf()
+        val attendances: MutableList<Map<Int, StudentAttendance>> = mutableListOf()
 
-        var offset: Int = 2
+        var offset = 2
         for (date in dates) {
             val (count, sumAttendances, renderLessons) = renderLessons(
                 context, date, students, offset
@@ -47,10 +49,10 @@ class ExportWeekViewModel : ViewModel() {
             result.add(renderLessons)
         }
 
-        val summedAttendance: Map<Int, LessonAttendance> = attendances
+        val summedAttendance: Map<Int, StudentAttendance> = attendances
             .fold(mutableMapOf()) { acc, map ->
                 map.forEach { (key, value) ->
-                    acc[key] = acc.getOrDefault(key, LessonAttendance()) + value
+                    acc[key] = acc.getOrDefault(key, StudentAttendance()) + value
                 }
                 acc
             }
@@ -70,17 +72,111 @@ class ExportWeekViewModel : ViewModel() {
     private suspend fun renderSummary(
         context: Context,
         students: List<StudentEntity>,
-        summedAttendance: Map<Int, LessonAttendance>,
+        summedAttendance: Map<Int, StudentAttendance>,
         offset: Int
     ): RenderData {
         TODO("Not yet implemented")
     }
 
-    private fun renderLessons(
-        context: Context, date: SimpleDate, students: List<StudentEntity>,
+    private suspend fun renderLessons(
+        context: Context, date: SimpleDate, allStudents: List<StudentEntity>,
         offset: Int
-    ): Triple<Int, Map<Int, LessonAttendance>, RenderData> {
-        TODO("Not yet implemented")
+    ): Triple<Int, Map<Int, StudentAttendance>, RenderData> {
+        val resultCells = mutableListOf<CellData>()
+        val resultBorders = mutableListOf<BorderData>()
+
+        val lessonListWithAttendance = withContext(Dispatchers.IO) {
+            StorageDependencies.lessonInfoRepository.getLessonsByDate(
+                date.day,
+                date.month,
+                date.year
+            ).map {
+                it to StorageDependencies.studentsAttendanceRepository.getStudentAttendanceWithNames(
+                    it.id
+                )
+            }
+        }
+
+        val studentsSum = mutableMapOf<Int, StudentAttendance>()
+
+        // Lessons with attendance
+        for ((lessonIndex, listPair) in lessonListWithAttendance.withIndex()) {
+            val (lesson, students) = listPair
+            resultCells.add(
+                CellData(
+                    lessonIndex,
+                    0,
+                    context.getString(
+                        lesson.type.toEdit().shortNameRes
+                    )
+                )
+            )
+            resultCells.add(
+                CellData(
+                    lessonIndex,
+                    1,
+                    lesson.name,
+                    rotation = 90
+                )
+            )
+            for (student in students) {
+                val studentIndex = allStudents.indexOfFirst {
+                    it.id == student.student.id
+                }
+                val toEdit = student.attendance.attendance.toEdit()
+                when (toEdit) {
+                    StudentAttendanceLesson.NULL -> studentsSum.compute(studentIndex) { _, oldValue ->
+                        oldValue ?: StudentAttendance()
+                    }
+
+                    StudentAttendanceLesson.VISIT -> studentsSum.compute(studentIndex) { _, oldValue ->
+                        oldValue ?: StudentAttendance()
+                    }
+
+                    StudentAttendanceLesson.NOT_VISIT ->
+                        studentsSum.compute(studentIndex) { _, oldValue ->
+                            (oldValue ?: StudentAttendance()) + StudentAttendance(0, 2)
+                        }
+
+                    StudentAttendanceLesson.SICK ->
+                        studentsSum.compute(studentIndex) { _, oldValue ->
+                            (oldValue ?: StudentAttendance()) + StudentAttendance(0, 2)
+                        }
+
+                    StudentAttendanceLesson.SICK_WITH_CERTIFICATE ->
+                        studentsSum.compute(studentIndex) { _, oldValue ->
+                            (oldValue ?: StudentAttendance()) + StudentAttendance(2, 0)
+                        }
+
+                    StudentAttendanceLesson.RESPECTFUL_PASS ->
+                        studentsSum.compute(studentIndex) { _, oldValue ->
+                            (oldValue ?: StudentAttendance()) + StudentAttendance(2, 0)
+                        }
+
+                    null -> studentsSum.compute(studentIndex) { _, oldValue ->
+                        oldValue ?: StudentAttendance()
+                    }
+                }
+                val skipped = toEdit?.displayNameRes?.let { context.getString(it) } ?: ""
+                resultCells.add(
+                    CellData(
+                        lessonIndex,
+                        studentIndex + 2,
+                        skipped
+                    )
+                )
+            }
+        }
+
+        return Triple(
+            lessonListWithAttendance.size,
+            studentsSum,
+            RenderData(
+                resultCells,
+                resultBorders,
+                offsetColumn = offset
+            )
+        )
     }
 
     private suspend fun renderStudentNames(context: Context): Pair<RenderData, List<StudentEntity>> {
