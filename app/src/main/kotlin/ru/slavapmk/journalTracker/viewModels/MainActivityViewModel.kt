ec1683a08f778e77dae27a0ae98a3792f4bc8dd0
Slava Petrov <slavapmk@gmail.com>
2025-03-14 13:12:07 +0300

    Refactor: Update exception handling in MainActivityViewModel
    This commit refactors the `MainActivityViewModel` to improve exception handling during the retrieval of the latest release version.
    The `UnknownHostException` was replaced with a more general `IOException` to catch a wider range of network-related issues.

--- app/src/main/kotlin/ru/slavapmk/journalTracker/viewModels/MainActivityViewModel.kt
+++ app/src/main/kotlin/ru/slavapmk/journalTracker/viewModels/MainActivityViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import ru.slavapmk.journalTracker.backend.RetrofitInstance
-import java.net.UnknownHostException
+import java.io.IOException
 
 data class VersionInfo(
     val tag: Int,
@@ -60,16 +60,16 @@ class MainActivityViewModel : ViewModel() {
 
     private suspend fun getLatestVersion(): VersionInfo? {
         try {
-        val releases = RetrofitInstance.githubApi.getReleases(REPO_OWNER, REPO_NAME)
-        val firstOrNull = releases.firstOrNull()
-        return firstOrNull?.let {
-            VersionInfo(
-                it.tagName.toInt(),
-                it.name,
-                it.htmlUrl
-            )
-        }
-        } catch (e: UnknownHostException) {
+            val releases = RetrofitInstance.githubApi.getReleases(REPO_OWNER, REPO_NAME)
+            val firstOrNull = releases.firstOrNull()
+            return firstOrNull?.let {
+                VersionInfo(
+                    it.tagName.toInt(),
+                    it.name,
+                    it.htmlUrl
+                )
+            }
+        } catch (e: IOException) {
             return null
         }
     }
